ec8ceb1032ffa26e6bbe9e9c54e08982513e3556
Laure-Anne Urbaniak <laureane.urbaniak@gmail.com>
2017-07-03 14:20:31 +0200

    Added Shifumi

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
+var joueur = prompt ("Quel est votre pseudo ?");
+var pointJoueur = 0;
+var pointOrdi = 0;
 
-      var joueur = prompt ("Quel est votre pseudo ?");
-      var pointJoueur = 0;
-      var pointOrdi = 0;
 
-
-     while (pointJoueur <3 && pointOrdi <3){
+while (pointJoueur <3 && pointOrdi <3 && jeu!="exit"){
         var ordi = Math.random()*3 ;
-        var jeu = prompt("Choisissez entre pierre, feuille ou ciseaux");
+        var jeu = prompt("Choisissez entre pierre, feuille ou ciseaux").toLowerCase();
 
         if (ordi<1){
           ordi="pierre";
@@ -14,20 +13,28 @@
         else if (ordi>1 && ordi<2){
           ordi="feuille";
         }
-        else{
+        else if (ordi>2){
           ordi="ciseaux";
         }
 
-
         if(jeu=="pierre" && ordi=="feuille" || jeu=="feuille" && ordi=="ciseaux" || jeu=="ciseaux" && ordi=="pierre"){
           pointOrdi++;
-          alert("Perdu\n" + joueur +" "+ pointJoueur + " - ordi " + pointOrdi);
+          alert("Perdu\n" + joueur +" "+ pointJoueur + " - ordi " + pointOrdi + "\nPour quitter le jeu, tapez 'exit'");
         }
-        if (jeu=="pierre" && ordi=="ciseaux" || jeu=="feuille" && ordi=="pierre" || jeu=="ciseaux" && ordi=="feuille" ){
+       	else if (jeu=="pierre" && ordi=="ciseaux" || jeu=="feuille" && ordi=="pierre" || jeu=="ciseaux" && ordi=="feuille" ){
           pointJoueur++;
-          alert("Gagné \n" + joueur +" "+ pointJoueur + " - ordi " + pointOrdi);
+          alert("Gagné \n" + joueur +" "+ pointJoueur + " - ordi " + pointOrdi  + "\nPour quitter le jeu, tapez 'exit'");
         }
-        if (jeu=="pierre" && ordi=="pierre" || jeu=="feuille" && ordi=="feuille" || jeu=="ciseaux" && ordi=="ciseaux" ){
-          alert("Nul !\n" + joueur +" "+ pointJoueur + " - ordi " + pointOrdi);
+        else if (jeu=="pierre" && ordi=="pierre" || jeu=="feuille" && ordi=="feuille" || jeu=="ciseaux" && ordi=="ciseaux" ){
+          alert("Nul !\n" + joueur +" "+ pointJoueur + " - ordi " + pointOrdi  + "\nPour quitter le jeu, tapez 'exit'");
         }
-     }
+        else if (jeu=="exit"){
+        	alert("Au revoir");
+       }
+       else if (jeu==""){
+       	alert('Vous devez choisir entre les 3 propositions !');
+       }
+       else {
+        alert('Veuillez entrer une donnée correcte !');
+       }
+  }
