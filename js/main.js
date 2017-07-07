2b766b2a11f1c5011ec0c6f5ba9225feba5b5ebc
Laure-Anne Urbaniak <laureane.urbaniak@gmail.com>
2017-07-07 16:42:02 +0200

    start page + show all the game content

--- js/main.js
+++ js/main.js
@@ -1,12 +1,59 @@
+var titre = document.getElementById('titre');
+var begin = document.getElementById('bouton');
+var form = document.getElementById('form');
+var pierre = document.getElementById('pierre');
+var feuille = document.getElementById('feuille');
+var ciseaux = document.getElementById('ciseaux');
+var ordi_pierre = document.getElementById('ordi_pierre');
+var ordi_feuille=document.getElementById('ordi_feuille');
+var ordi_ciseaux=document.getElementById('ordi_ciseaux');
+
+
+titre.style.display='none';
+form.style.visibility ='hidden';
+pierre.style.visibility = 'hidden';
+feuille.style.visibility = 'hidden';
+ciseaux.style.visibility = 'hidden';
+ordi_pierre.style.visibility = 'hidden';
+ordi_feuille.style.visibility = 'hidden';
+ordi_ciseaux.style.visibility = 'hidden';
+
+begin.onclick=function(){
+  begin.style.display='none';
+  titre.style.display ='block';
+  form.style.visibility = 'visible';
+  pierre.style.visibility = 'visible';
+  feuille.style.visibility = 'visible';
+  ciseaux.style.visibility = 'visible';
+  ordi_pierre.style.visibility = 'visible';
+  ordi_feuille.style.visibility = 'visible';
+  ordi_ciseaux.style.visibility = 'visible';
+}
+
+
+
 var pseudo = document.getElementById('pseudo');
 var ok = document.getElementById('ok');
 var bienvenue = document.getElementById('bienvenue');
+var player_name = document.getElementById('player_name');
 var value;
 
 ok.onclick=function(){
 value=pseudo.value;
-bienvenue.innerHTML= "Bienvenue " + value ;
+bienvenue.innerHTML= "Bienvenue " + value.toUpperCase();
+player_name.innerHTML= value.toUpperCase() ;
+pseudo.style.display='none';
+ok.style.display= 'none';
 }
+
+
+var pierre = document.getElementById('pierre');
+var feuille = document.getElementById('feuille');
+var ciseaux = document.getElementById('ciseaux');
+var pointJoueur = 0;
+var pointOrdi = 0;
+var ordi = Math.random()*3 ;
+
 
 
 // function myFunction(){
